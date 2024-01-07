b7242d0d456395b2cf184dc7884fbe631e143b01
m1-dev <140819896+m1-dev@users.noreply.github.com>
2024-01-07 09:45:47 +0200

    feat: create deployment method

--- src/client/managers/Deployments.ts
+++ src/client/managers/Deployments.ts
@@ -1,10 +1,32 @@
 import { BaseManager } from './Base';
-import type { ListDeploymentsParams, DeploymentData } from '../../types';
+import type {
+  ListDeploymentsParams,
+  DeploymentData,
+  CreateDeploymentParams,
+  CreateDeploymentData
+} from '../../types';
 
 import Routes from '../../routes';
 import axios from 'axios';
 
 export class DeploymentsManager extends BaseManager {
+  /**
+   * Create a new deployment with all the required and intended data.
+   * If the deployment is not a git deployment, all files must be provided with
+   * the request, either referenced or inlined. Additionally, a deployment id
+   * can be specified to redeploy a previous deployment.
+   * @see https://vercel.com/docs/rest-api/endpoints#create-a-new-deployment
+   */
+  public async create(params: CreateDeploymentParams): Promise<CreateDeploymentData> {
+    const deployment = await axios.post(Routes.DEPLOYMENTS.CREATE(params), params, {
+      headers: {
+        Authorization: this.client.token
+      }
+    });
+
+    return deployment.data;
+  }
+
   /**
    * List deployments under the authenticated user or team.
    * If a deployment hasn't finished uploading (is incomplete), the `url` property will have a value of `null`.
