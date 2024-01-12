1fce6d560654ac3150be088c8ce01ae45f02b372
m1-dev <140819896+m1-dev@users.noreply.github.com>
2024-01-12 15:14:19 +0200

    docs: add favicon

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -5,7 +5,20 @@ function Logo() {
   return <strong className='font-bold'>Vercel.js</strong>;
 }
 
+function Head() {
+  return (
+    <>
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <meta property='og:title' content='Vercel.js' />
+      <meta property='og:description' content='Node.js module for the Vercel API' />
+      <link rel='icon' href='/favicon.png' type='image/x-icon' />
+    </>
+  );
+}
+
 export default {
+  head: Head(),
+  logo: Logo(),
   project: {
     link: 'https://github.com/m1-dev/Vercel.js'
   },
@@ -15,6 +28,5 @@ export default {
   docsRepositoryBase: 'https://github.com/m1-dev/Vercel.js/tree/main/apps/docs',
   footer: {
     text: 'Built with Nextra by M1.'
-  },
-  logo: Logo()
+  }
 } satisfies DocsThemeConfig;
